b75c44221af2d7035f1e64e7b2bd81e490d4a675
Hmida Rojbani <31728681+Hmida-Rojbani@users.noreply.github.com>
2021-10-11 15:42:08 +0100

    getById

--- src/main/java/de/tekup/first/controllers/EmployeeCtrl.java
+++ src/main/java/de/tekup/first/controllers/EmployeeCtrl.java
@@ -1,8 +1,14 @@
 package de.tekup.first.controllers;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,6 +31,25 @@ public class EmployeeCtrl {
 	@PostMapping("/employee/add")
 	public EmployeeEntity saveEmployeeEntity(@RequestBody EmployeeEntity employeeEntity) {
 		return empService.addToDB(employeeEntity);
+	}
+	
+	@GetMapping("/employee/get/{id}")
+	public EmployeeEntity getEmployeeById(@PathVariable int id) {
+		return empService.getById(id);
+	}
+	
+	@ExceptionHandler(DataIntegrityViolationException.class)
+	public ResponseEntity<String> handleDataIntegrityViolationException(DataIntegrityViolationException e){
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+							.body("Erreur in saving in DB :"+e.getMessage());
+				
+	}
+	
+	@ExceptionHandler(NoSuchElementException.class)
+	public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e){
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+							.body(e.getMessage());
+				
 	}
 
 }
--- src/main/java/de/tekup/first/services/EmployeeService.java
+++ src/main/java/de/tekup/first/services/EmployeeService.java
@@ -1,6 +1,8 @@
 package de.tekup.first.services;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +23,16 @@ public class EmployeeService {
 
 	public EmployeeEntity addToDB(EmployeeEntity employeeEntity) {
 		return empRepos.save(employeeEntity);
+	}
+
+	public EmployeeEntity getById(int id) {
+		Optional<EmployeeEntity> opt =  empRepos.findById(id);
+		
+//		if(opt.isPresent())
+//			return opt.get();
+//		throw new NoSuchElementException("Employee with this id not found");
+		
+		return opt.orElseThrow(()->new NoSuchElementException("Employee with this id not found"));
 	}
 	
 
